fe596add498ff74518033f5d84d23692406fe049
Baktiar Miah <baktiar.m@gmail.com>
2019-08-14 20:40:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(speak)
   if 
     speak == "I LOVE YOU GRANDMA!"
-   return 'I LOVE YOU TOO PUMPKIN'
+   return 'I LOVE YOU TOO PUMPKIN!'
    
   elsif
     speak == speak.upcase
