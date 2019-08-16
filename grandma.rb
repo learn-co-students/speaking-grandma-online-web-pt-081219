117f6a7665d5868281213aae8b9864528adf91cd
Steven Laff <steve.laff@gmail.com>
2019-08-16 22:30:10 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(greeting)
   
   if greeting == "I LOVE YOU GRANDMA!"
-    p "I LOVE YOU TOO PUMPKIN!"
-  elsif greeting.upcase
-    p "NO, NOT SINCE 1938!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif greeting == greeting.upcase
+    return "NO, NOT SINCE 1938!"
   else
-    p "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 
 end
