a6c648e5803f1baa6ad59d1448bde532761f9832
Oksana Nichols <oksanahnichols@yahoo.com>
2019-09-20 02:27:36 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,6 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+ it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
